98dada56cee1d15af6c12342f5dca2e69f54aa51
Siddharth kumar <180748912+UndefinedSid@users.noreply.github.com>
2026-02-04 22:25:51 +0530

    Add files via upload

---
+++ 1881A.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n,m;
+        cin >> n >> m;
+        string strX,strS;
+
+        cin >> strX;
+        cin >> strS;
+
+        int ops=-1;
+        
+        for(int k=0;k<=20;k++){
+            long long copies=1LL << k;
+            long long target=min <long long>(copies * n,m + 2LL * n);
+            string curr;
+            curr.reserve((size_t)target);
+            long long added=0;
+
+            while(added < target){
+                curr += strX;
+                added += n;
+            }
+            if(curr.find(strS) != string::npos){
+                ops=k;
+                break;
+            }
+        }
+        cout << ops << '\n';
+
+    //     bool found=false;
+
+    //     while((int)strX.size() <= 2 * m){
+    //         if(strX.find(strS) != string::npos){
+    //             cout << ops << '\n';
+    //             found=true;
+    //             break;
+    //         }
+    //         strX += strX;
+    //         ops++;
+    //     }
+
+    //     if( ! found){
+    //         if(strX.find(strS) != string::npos)
+    //             cout << ops << "\n";
+    //         else
+    //             cout << -1 << "\n";
+    //     }
+
+    }
+
+    return 0;
+}
