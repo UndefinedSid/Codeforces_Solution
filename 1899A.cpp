450a57a96f55f25c11b2005c73709a5cf8b3282a
Siddharth kumar <180748912+UndefinedSid@users.noreply.github.com>
2026-02-06 21:58:10 +0530

    Update 1899A.cpp
    Added Python code as well

--- 1899A.cpp
+++ 1899A.cpp
@@ -21,5 +21,18 @@ int main(){
     return 0;
 }
 
+// python code
+
+def main():
+    t=int(input())
+    for _ in range(t):
+        n=int(input())
+        if n % 3==0:
+            print("Second")
+        else:
+            print("First")
+    
+if __name__== "__main__":
+    main()
 
 
