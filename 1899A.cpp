2160823322e09fe47f73f6ad962e8cd9cf6292d2
Siddharth kumar <180748912+UndefinedSid@users.noreply.github.com>
2026-02-06 21:52:50 +0530

    Create 1899A.cpp

---
+++ 1899A.cpp
@@ -0,0 +1,25 @@
+Problem link --> https://codeforces.com/problemset/problem/1899/A
+
+// C++ code
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+
+        if(n % 3==0){
+            cout << "Second" << '\n';
+        }else {
+            cout << "First" << '\n';
+        }
+    
+    }
+    return 0;
+}
+
+
+
