c63a43596e4e67b4c09f04bf5d838e2b91053128
Siddharth kumar <180748912+UndefinedSid@users.noreply.github.com>
2026-03-17 00:55:55 +0530

    Add files via upload

---
+++ 1783A.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for(int i=0;i<n;i++){
+            cin >> arr[i];
+        }
+        sort(arr.begin(),arr.end());
+        int mini=arr[0];
+        int maxi=arr[n-1];
+        if(mini==maxi)
+            cout << "NO" << endl;
+        else{
+            cout << "YES" << endl;
+            cout << maxi << " ";
+            for(int i=0;i<n-1;i++)
+                cout << arr[i] << " ";
+            
+                cout << endl;
+        }
+    }
+    return 0;
+}
