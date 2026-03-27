19725dbe6668fb7dc9c07cb41dd1372a70dde8b2
Siddharth kumar <180748912+UndefinedSid@users.noreply.github.com>
2026-03-27 11:41:08 +0530

    Add files via upload

---
+++ 1858A.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        long long a,b,c;
+        cin >> a >> b >> c;
+        if(c % 2==1){
+            if(b > a)
+                cout << "Second" << endl;
+            else
+                cout << "First" << endl;
+        }else {
+            if(a > b)
+                cout << "First" << endl;
+            else
+                cout << "Second" << endl;
+        }
+        // long long sum =a+ b+c;
+        // if(sum % 2==0)
+        //     cout << "Second" << '\n';
+        // else
+        //     cout << "First" << '\n';
+    }
+    return 0;
+}
