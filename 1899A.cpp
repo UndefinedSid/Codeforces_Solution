8ba1bd1859ab606d134e891e7a0c99335e0b68f1
Siddharth kumar <180748912+UndefinedSid@users.noreply.github.com>
2026-03-29 23:52:48 +0530

    Add files via upload

--- 1899A.cpp
+++ 1899A.cpp
@@ -1,6 +1,3 @@
-Problem link --> https://codeforces.com/problemset/problem/1899/A
-
-// C++ code
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -20,19 +17,3 @@ int main(){
     }
     return 0;
 }
-
-// python code
-
-def main():
-    t=int(input())
-    for _ in range(t):
-        n=int(input())
-        if n % 3==0:
-            print("Second")
-        else:
-            print("First")
-    
-if __name__== "__main__":
-    main()
-
-
