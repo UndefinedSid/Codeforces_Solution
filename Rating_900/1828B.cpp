d41457605e2aeb90665aab5805e5c25068e7872b
Siddharth kumar <180748912+UndefinedSid@users.noreply.github.com>
2026-04-05 22:48:50 +0530

    Add files via upload

---
+++ Rating_900/1828B.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int findGcd(int a, int b){
+    while(b){
+        int temp = a % b;
+        a = b;
+        b = temp;
+    }
+    return a;
+}
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for(int i=0;i<n;i++)
+            cin >> arr[i];
+
+        int k=abs(arr[0]-1);
+        for(int i=1;i<n;i++){
+            k=findGcd(k,abs(arr[i]-(i+1)));
+        }
+        cout << k << endl;
+    }
+    return 0;
+}
