a2c2a5e8e5d1ed19aa437aa2fec84a4eee68dabd
Siddharth kumar <180748912+UndefinedSid@users.noreply.github.com>
2026-04-07 22:10:23 +0530

    Add files via upload

---
+++ Rating_900/1875A.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll =long long;
+
+int main()
+{
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        ll a, b, n;
+        if (!(cin >> a >> b >> n))
+            return 0;
+
+        vector<ll> x(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> x[i];
+        }
+
+        ll ans = b;
+        for (int i = 0; i < n; i++)
+        {
+            ans += min(x[i], a - 1);
+        }
+
+        cout << ans << "\n";
+    }
+    return 0;
+}
