bf79338ef27ebefbebdd0a2a555ee1272fd60fd2
Siddharth kumar <180748912+UndefinedSid@users.noreply.github.com>
2026-04-23 00:44:17 +0530

    Add files via upload

---
+++ Rating_900/1883B.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long t;
+    cin >> t;
+    while (t--) {
+        long long n, k;
+        cin >> n >> k;
+        string s;
+        cin >> s;
+
+        vector<int> freq(26, 0);
+        for (int i = 0; i < n; i++) {
+            freq[s[i] - 'a']++;
+        }
+
+        long long oddCount = 0;
+        for (int i = 0; i < 26; i++) {
+            oddCount += freq[i] % 2;
+        }
+
+        if (oddCount > k + 1)
+            cout << "NO\n";
+        else
+            cout << "YES\n";
+    }
+    return 0;
+}
