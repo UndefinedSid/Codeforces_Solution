77772a9847335d4185e3dd0873446c82f76aa806
Siddharth kumar <180748912+UndefinedSid@users.noreply.github.com>
2026-04-28 22:25:47 +0530

    Add files via upload

---
+++ Rating_900/1666D.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    while(n--){
+        string s,t;
+        cin >> s >> t;
+        map<char,int> need;
+        for(char c : t)
+            need[c]++;
+        
+        string ans="";
+        for(int i=s.size()-1;i>=0;i--){
+            char ch=s[i];
+            if(need[ch] > 0){
+                ans += ch;
+                need[ch]--;
+            }
+        }
+        reverse(ans.begin(),ans.end());
+        if(ans==t)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+    return 0;
+}
