a439ac80e9746b25b7804a91e90f3153154de2f9
Siddharth kumar <180748912+UndefinedSid@users.noreply.github.com>
2026-05-03 22:58:15 +0530

    Add files via upload

---
+++ Rating_900/1665B.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long tc;
+    cin >> tc;
+    while (tc--) {
+        long long sz;
+        cin >> sz;
+        vector<long long> v(sz);
+        map<long long, long long> counts;
+        
+        for (int i = 0; i < sz; i++) {
+            cin >> v[i];
+            counts[v[i]]++;
+        }
+
+        long long max_f = 0;
+        for (auto const& [val, freq] : counts) {
+            if (freq > max_f) {
+                max_f = freq;
+            }
+        }
+
+        long long ans = 0;
+        while (max_f < sz) {
+            long long remaining = sz - max_f;
+            ans++; 
+            
+            if (remaining >= max_f) {
+                ans += max_f;
+                max_f *= 2;
+            } else {
+                ans += remaining;
+                max_f = sz;
+            }
+        }
+        cout << ans << "\n";
+    }
+    return 0;
+}
