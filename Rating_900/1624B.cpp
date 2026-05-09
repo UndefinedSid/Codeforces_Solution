102c14315a74f9b7ffbe8474d9adc8c0bbeb6e39
Siddharth kumar <180748912+UndefinedSid@users.noreply.github.com>
2026-05-09 23:42:18 +0530

    Add files via upload

---
+++ Rating_900/1624B.cpp
@@ -0,0 +1,32 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+using ll =long long;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        ll a,b,c;
+        cin >> a >> b >> c;
+        bool ans=false;
+
+        ll newA=2*b-c;
+        if(newA / a > 0 && newA % a ==0)
+            ans=true;
+
+        ll newB=(a+c)/2;
+        if(newB / b > 0 && newB % b==0 && (c-a) % 2 ==0)
+            ans=true;
+
+        ll newC=2*b-a;
+        if(newC / c > 0 && newC % c==0 )
+            ans=true;
+
+        if(ans)           
+         cout << "YES\n";
+        else
+         cout << "NO\n";
+    }
+    return 0;
+}
